f5548dcb2fbc14939b3a127afdb4c8aaa34ab395
Kanmii <maneptha@gmail.com>
2018-05-21 01:36:24 +0200

    Rust Run Length Encoding 2

--- rust/run-length-encoding/src/lib.rs
+++ rust/run-length-encoding/src/lib.rs
@@ -1,40 +1,60 @@
 pub fn encode(source: &str) -> String {
-    let start: Vec<(usize, char)> = Vec::new();
-
-    source
-        .chars()
-        .fold(start, move |acc, curr| {
-            let mut acc = acc;
-
-            match acc.pop() {
-                None => {
-                    acc.push((1, curr));
-                    acc
-                }
-                Some((count, c)) if c == curr => {
-                    acc.push((count + 1, curr));
-                    acc
-                }
-                Some((count, c)) => {
-                    acc.push((count, c));
-                    acc.push((1, curr));
-                    acc
-                }
-            }
-        })
-        .into_iter()
-        .fold(String::new(), |acc, (count, c)| match count {
-            1 => format!("{}{}", acc, c),
-            n => format!("{}{}{}", acc, n, c),
-        })
+    let mut e = Encode::new();
+    for c in source.chars() {
+        e.encode(c);
+    }
+    e.to_string()
 }
 
 pub fn decode(source: &str) -> String {
     let mut d = Decode::new();
     for c in source.chars() {
-        d.trans(c);
+        d.decode(c);
     }
     d.letters
+}
+
+struct Encode {
+    curr: char,
+    letters: Vec<(usize, char)>,
+    last: i32,
+}
+
+impl Encode {
+    fn new() -> Encode {
+        Encode {
+            curr: '"',
+            letters: vec![],
+            last: -1,
+        }
+    }
+
+    fn encode(&mut self, c: char) -> () {
+        match self.curr == c {
+            true => {
+                let (count, _) = self.letters[self.last as usize];
+                self.letters[self.last as usize] = (count + 1, c);
+            }
+            _ => {
+                self.curr = c;
+                self.letters.push((1, c));
+                self.last += 1;
+            }
+        }
+    }
+
+    fn to_string(self) -> String {
+        let mut result = String::new();
+
+        for (count, c) in self.letters {
+            match count {
+                1 => result.push(c),
+                _ => result.push_str(&format!("{}{}", count, c)),
+            }
+        }
+
+        result
+    }
 }
 
 struct Decode {
@@ -50,7 +70,7 @@ impl Decode {
         }
     }
 
-    fn trans(&mut self, c: char) {
+    fn decode(&mut self, c: char) {
         match (char::is_numeric(c), self.num.is_empty()) {
             (true, _) => self.num.push(c),
             (_, true) => self.letters.push(c),
