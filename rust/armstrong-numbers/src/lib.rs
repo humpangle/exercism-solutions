fd422e696bf89b57fb62c793078804f970270557
Kanmii <maneptha@gmail.com>
2018-05-20 16:05:36 +0200

    Rust Armstrong Numbers 2

--- rust/armstrong-numbers/src/lib.rs
+++ rust/armstrong-numbers/src/lib.rs
@@ -1,7 +1,6 @@
 pub fn is_armstrong_number(num: u32) -> bool {
     let num_ = num.to_string();
     let len = num_.len() as u32;
-    num_.chars().fold(0, |acc, n| {
-        acc + n.to_string().parse::<u32>().unwrap().pow(len)
-    }) == num
+    num == num_.chars()
+        .fold(0, |acc, n| acc + n.to_digit(10).unwrap().pow(len))
 }
