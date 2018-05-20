d387ac9c61425f0aff7533d5cfe9c5b754979d5f
Kanmii <maneptha@gmail.com>
2018-05-20 15:31:12 +0200

    Rust Series 2

--- rust/series/src/lib.rs
+++ rust/series/src/lib.rs
@@ -1,14 +1,11 @@
 pub fn series(digits: &str, len: usize) -> Vec<String> {
-    let mut result: Vec<String> = Vec::new();
-    match digits.len() as i32 - len as i32 {
-        last if last < 0 => result,
-        last => {
-            for i in 0..=(last as usize) {
-                let x = (&digits[i..(len + i)]).to_string();
-                result.push(x);
-            }
-
-            result
-        }
+    match len <= 0 {
+        true => vec!["".to_string(); digits.len() + 1],
+        _ => digits
+            .chars()
+            .collect::<Vec<_>>()
+            .windows(len)
+            .map(|chars| chars.iter().collect::<String>())
+            .collect(),
     }
 }
