d12827b31f7c7c8443ba920bf74d48467772b984
Kanmii <maneptha@gmail.com>
2018-10-22 13:40:04 +0200

    Mix tasks to automate elixir things.

--- elixir/lib/mix/tasks/exercism.ex
+++ elixir/lib/mix/tasks/exercism.ex
@@ -1,17 +1,88 @@
 defmodule Mix.Tasks.Exercism do
+  @win_cmd "cmd.exe"
+
   @spec run([String.t(), ...]) :: :ok
   def run(args), do: task(args)
 
-  defp task(["ex_to_exs", path]) do
-    cond do
-      not File.exists?(path) ->
-        raise "I can not find the given file: '#{path}'"
+  defp task(["test", path]) do
+    ext = Path.extname(path)
 
-      Path.extname(path) != ".ex" ->
-        raise "File must be a '.ex' file"
+    cond do
+      not valid_elixir_file?(ext) ->
+        raise "File must be a '.ex' or '.exs' file"
 
       true ->
-        File.write!(Path.rootname(path) <> ".exs", File.read!(path))
+        root = Path.rootname(path)
+
+        cond do
+          String.ends_with?(root, "test") ->
+            run_cmd("mix", ["test", path])
+
+          ext == ".exs" ->
+            run_cmd("mix", ["test", "#{root}_test.exs"])
+
+          ext == ".ex" ->
+            File.write!(root <> ".exs", File.read!(path))
+            run_cmd("mix", ["test", "#{root}_test.exs"])
+        end
     end
+  end
+
+  defp task(["submit", path]) do
+    ext = Path.extname(path)
+
+    cond do
+      not valid_elixir_file?(ext) ->
+        raise "File must be a '.ex' or '.exs' file"
+
+      true ->
+        root =
+          path
+          |> Path.rootname()
+          |> String.replace("_test", "")
+
+        exs_path = "#{root}.exs"
+        File.write!(exs_path, File.read!(root <> ".ex"))
+
+        run_cmd(
+          "exercism",
+          ["submit", exs_path]
+        )
+    end
+  end
+
+  defp task(["exs-to-ex", path]) do
+    ext = Path.extname(path)
+
+    cond do
+      ext != ".exs" ->
+        raise "File must be a '.exs' file"
+
+      true ->
+        File.write!(Path.rootname(path) <> ".ex", File.read!(path))
+    end
+  end
+
+  defp valid_elixir_file?(ext), do: Enum.member?([".ex", ".exs"], ext)
+
+  defp run_cmd(command, args), do: run_cmd(command, args, [])
+
+  defp run_cmd(command, args, opts) do
+    {os_family, _} = :os.type()
+
+    {command, args} =
+      case Atom.to_string(os_family) =~ "win" do
+        true ->
+          args = ["/c", command | args]
+          command = @win_cmd
+          {command, args}
+
+        _ ->
+          {command, args}
+      end
+
+    {cmd, _status} = System.cmd(command, args, opts)
+
+    Mix.shell().info(cmd)
   end
 end
