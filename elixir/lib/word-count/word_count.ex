0689bffc6c443be1113e8508c379b07cab06bc45
Kanmii <maneptha@gmail.com>
2018-11-19 08:07:07 +0100

    Elixir word count - adjusted.

--- elixir/lib/word-count/word_count.ex
+++ elixir/lib/word-count/word_count.ex
@@ -1,6 +1,5 @@
 defmodule Words do
-  @split_pattern ~r/[\s_]/
-  @word_pattern ~r/[\w\d]/
+  @split_pattern ~r/(_|[^\w-])/u
 
   @doc """
   Count the number of words in the sentence.
@@ -10,28 +9,8 @@ defmodule Words do
   @spec count(String.t()) :: map
   def count(sentence) do
     sentence
-    |> String.split(@split_pattern)
-    |> Enum.reduce(Map.new(), fn raw_word, acc ->
-      case check(String.codepoints(raw_word), "") do
-        "" -> acc
-        word -> Map.update(acc, word, 1, &(&1 + 1))
-      end
-    end)
-  end
-
-  defp check([], word) do
-    String.downcase(word)
-  end
-
-  defp check(["-" | words], word), do: check(words, <<word::binary, "-">>)
-
-  defp check([l | words], word) do
-    case Regex.match?(@word_pattern, l) do
-      true ->
-        check(words, <<word::binary, l::binary>>)
-
-      _ ->
-        check(words, word)
-    end
+    |> String.downcase()
+    |> String.split(@split_pattern, trim: true)
+    |> Enum.reduce(%{}, &Map.update(&2, &1, 1, fn count -> count + 1 end))
   end
 end
